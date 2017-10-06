7b33d59d337922dfb849c1b10d2ed380594b04f7
acejudo <adriraziel@gmail.com>
2017-10-06 17:49:50 +0200

    First Unit

---
+++ 1-instalacion-data-binding/js/main.js
@@ -0,0 +1,16 @@
+// Vanilla Javascript
+
+//const input = document.querySelector('input');
+
+//const h1 = document.querySelector('h1');
+
+//input.addEventListener('keyup', () =>
+//    h1.innerHTML = input.value
+//);
+
+new Vue({
+    el:'main',  // accede dentro de la etiqueta de main
+    data:{      // especifica el modelo
+        mensaje: 'hola mundo!'
+    }
+});
