81817a18d68bb624d9129acba558d9ef9cdd4e1f
acejudo <adriraziel@gmail.com>
2017-10-06 19:39:42 +0200

    3er video

---
+++ 3-renderizado-de-listas/js/main.js
@@ -0,0 +1,16 @@
+const vm = new Vue({
+    el:'main',
+    data:{
+        nombre:'Adri',
+        laborales:['Lunes','Martes','Miercoles'],
+        tareas:[
+            {nombre:"tarea1", valor:"otra cosa"},
+            {nombre:"tarea2", valor:"otra cosa"},
+        ],
+        persona:{
+            nombre:'Adri',
+            profesion:'Dev',
+            ciudad:'Leon'
+        }
+    },
+})
